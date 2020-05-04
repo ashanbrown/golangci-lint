9e847a38d406a36f2268d312a0cd4881d7c6ce04
Andrew S. Brown <andrew@launchdarkly.com>
2020-05-03 21:26:46 -0700

    Add test for goconst in nolintlint

--- test/testdata/nolintlint_unused.go
+++ test/testdata/nolintlint_unused.go
@@ -1,4 +1,4 @@
-//args: -Enolintlint -Evarcheck
+//args: -Enolintlint -Evarcheck -Egoconst
 //config: linters-settings.nolintlint.allow-unused=false
 package testdata
 
@@ -8,4 +8,8 @@ func Foo() {
 	fmt.Println("unused")          //nolint // ERROR "directive `//nolint .*` is unused"
 	fmt.Println("unused,specific") //nolint:varcheck // ERROR "directive `//nolint:varcheck .*` is unused for linter varcheck"
 	fmt.Println("not run")         //nolint:unparam // unparam is not run so this is ok
+
+	fmt.Println("same")
+	fmt.Println("same")
+	fmt.Println("same")
 }
